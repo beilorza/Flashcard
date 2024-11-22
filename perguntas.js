92a97c31e37c24df2cd43b3b27c0eb0756cb8295
beilorza <00001055333277sp@al.educacao.sp.gov.br>
2024-11-22 14:00:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Curiosidades',
+    'Quando iniciou a carreira?',
+    'Os músicos ganharam popularidade inicial em 2016, com o lançamento de Ao Vivo em São José do Rio Preto, que conteve a música "Seu Polícia".'
+)
+
+criaCartao(
+    'Curiosidades José Neto e Cristiano ',
+    'Cantam qual estilo de música?',
+    'Sertanejo'
+)
+
+criaCartao(
+    'Curiosidades José Neto e Cristiano',
+    'Qual a profissão deles anteriormente?',
+    'Ao ser questionado sobre as suas profissões antes da carreira musical, Zé Neto revelou: "Tive muitas profissões! Fui tratorista, lavageiro e servente de pedreiro", finalizou o cantor.'
+)
+
+criaCartao(
+    'Curiosidades José Neto e Cristiano',
+    'Sobre os artistas?',
+    'Ao longo de sua carreira, Zé Neto e Cristiano foram coroados com alguns dos maiores prêmios da imprensa brasileira, sendo eles: Melhores do Ano: Música do Ano (2018) Troféu Imprensa: Melhor Dupla Sertaneja (2019) Troféu Internet: Melhor Dupla Sertaneja (2019)'
+)
